8a709dcdcf967b7b54239e0b99b21afac7a798c9
NickH <nickh@datacom.co.nz>
2019-11-23 16:11:39 +1300

    Initial Commit

---
+++ FB2019_lbta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FB2019_lbta
+//
+//  Created by Nick He on 23/11/19.
+//  Copyright © 2019 Nick He. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
